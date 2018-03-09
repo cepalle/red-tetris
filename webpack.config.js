6e1e7f2bacd41bd6afde8dd79b3cfa340fada1a0
Epalle <cyrille.epalle@epita.fr>
2018-03-09 10:05:38 +0100

    move file and start server config

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./src/index.html",
+      template: "./src/client/index.html",
       filename: "./index.html"
     })
   ]
