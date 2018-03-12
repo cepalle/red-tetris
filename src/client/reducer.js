7aeabfe51dc3c53430d16c76d1b344791b1de9fa
cepalle <epalle.cyrille@gmail.com>
2018-03-12 16:21:13 +0100

    parts.js add

--- src/client/reducer.js
+++ src/client/reducer.js
@@ -1,4 +1,40 @@
-const initialState = {};
+import {PARTS} from '../parts';
+
+const grid_init = () => {
+  const grid = [];
+  for (let i = 0; i < 20; i++) {
+    grid.push([]);
+    for (let j = 0; j < 10; j++) {
+      grid.push(0);
+    }
+  }
+  return grid;
+};
+
+
+const exemple = {
+  grids: [
+    {
+      grid: grid_init(),
+      player_name: "name",
+    },
+  ],
+  player_name: "name",
+  room_name: "room",
+  is_master: true,
+  parts_flow: [PARTS._1, PARTS._2, PARTS._1, PARTS._5, PARTS._6, PARTS._1],
+  cur_part_pos: {x: 0, y: 0},
+};
+
+
+const initialState = {
+  grids: [],
+  player_name: "",
+  room_name: "",
+  is_master: true,
+  parts_flow: [],
+  cur_part_pos: undefined,
+};
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
