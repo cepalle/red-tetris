d38f03ecb5676918c480b33cc04e19ecaf13d11d
cepalle <epalle.cyrille@gmail.com>
2018-03-12 15:33:56 +0100

    add files

---
+++ src/client/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <p>
+        React here!
+      </p>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/client/reducer.js
@@ -0,0 +1,10 @@
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return state.concat([action.text]);
+    default:
+      return state;
+  }
+};
+
+export {reducer};
