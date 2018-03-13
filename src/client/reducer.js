772a6ad2db2d6271ef06ccf6b83d52b7e090c05b
Epalle <cyrille.epalle@epita.fr>
2018-03-13 21:25:33 +0100

    url_handler add

--- src/client/reducer.js
+++ src/client/reducer.js
@@ -1,8 +1,7 @@
 import {PARTS} from '../common/parts';
+import {url_get_player_name, url_get_room_name} from "./url_handler"
 
-{window.location.href}
-
-const exemple = {
+const initialState = {
   grids: [
     {
       grid: [
@@ -26,11 +25,11 @@ const exemple = {
         [1, 2, 7, 7, 5, 5, 3, 4, 4, 6],
         [1, 2, 2, 2, 3, 3, 3, 4, 4, 0],
       ],
-      player_name: "name",
+      player_name: url_get_player_name(),
     },
   ],
-  player_name: window.location.href.split("[")[1].split("]")[0], // change ?
-  room_name: window.location.href.split("#")[1].split("[")[0],
+  player_name: url_get_player_name(), // function
+  room_name: url_get_room_name(),
   is_master: true,
   parts_flow: [PARTS._1, PARTS._2, PARTS._1, PARTS._5, PARTS._6, PARTS._1],
   cur_part_pos: {x: 0, y: 5},
@@ -47,16 +46,7 @@ const grid_init = () => {
   return grid;
 };
 
-const initialState = {
-  grids: [],
-  player_name: "",
-  room_name: "",
-  is_master: true,
-  parts_flow: [],
-  cur_part_pos: undefined,
-};
-
-const reducer = (state = exemple, action) => {
+const reducer = (state = initialState, action) => {
   console.log(action);
   console.log(state);
   switch (action.type) {
