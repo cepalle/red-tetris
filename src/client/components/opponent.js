3af7eda077edf7037182b2b5fd2b590bc38512a8
cepalle <epalle.cyrille@gmail.com>
2018-03-20 09:52:47 +0100

    display specter oponnent

--- src/client/components/opponent.js
+++ src/client/components/opponent.js
@@ -1,27 +1,44 @@
 import React from "react";
 import {connect} from 'react-redux';
 import {clonePlayerStates, getColorNum} from "../util/utils"
+import {GRID_HEIGHT, GRID_WIDTH} from "../redux/init-state";
 
-const OpponentComponent = ({states}) =>
-  <div className={"line"}>
-    {states.map((state, k) =>
-      <div key={k}>
-        <div className={"grid"}>
-          {state.grid.map((line, i) => i >= 4 &&
-            <div key={i} className={"line"}>
-              {line.map((el, j) =>
-                <div key={j} className={"case color" + getColorNum(el)}/>
-              )}
-            </div>
-          )}
-        </div>
-        <div className={"line center"}>
-          <p>{state.playerName}{state.isMaster && "(Master)"}{state.hasLoose && "(loose)"}{state.hasWin && "(Win)"}</p>
-        </div>
-      </div>
+const OpponentComponent = ({states}) => {
+  return <div className={"line"}>
+    {states.map((state, k) => {
+        const grid = state.grid.map(l => l.map(e => e));
+
+        for (let i = 0; i < GRID_WIDTH; i++) {
+          let obstacle = false;
+          for (let j = 0; j < GRID_HEIGHT; j++) {
+            if (grid[j][i] !== 0) {
+              obstacle = true;
+            }
+            if (obstacle) {
+              grid[j][i] = 7;
+            }
+          }
+        }
+
+        return <div key={k}>
+          <div className={"grid"}>
+            {grid.map((line, i) => i >= 4 &&
+              <div key={i} className={"line"}>
+                {line.map((el, j) =>
+                  <div key={j} className={"case color" + getColorNum(el)}/>
+                )}
+              </div>
+            )}
+          </div>
+          <div className={"line center"}>
+            <p>{state.playerName}{state.isMaster && "(Master)"}{state.hasLoose && "(loose)"}{state.hasWin && "(Win)"}</p>
+          </div>
+        </div>;
+      }
     )}
   </div>
-;
+    ;
+};
 
 const mapStateToProps = state => {
   return {states: clonePlayerStates(state.playerStates).filter(el => el.playerName !== state.playerName)};
