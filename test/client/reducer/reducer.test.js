03726679b457cc1be669dfc5c189a0dc488d0125
cepalle <epalle.cyrille@gmail.com>
2018-03-21 17:46:35 +0100

    add store test

--- test/client/reducer/reducer.test.js
+++ test/client/reducer/reducer.test.js
@@ -1,7 +1,12 @@
 import {store} from "../../../src/client/middlewares/store";
-import {addPiecesFlow} from "../../../src/client/actions/action-creators";
+import {
+  addError, addPiecesFlow, addWallLine, movePiece, startGame, updateGrid,
+  updatePlayers
+} from "../../../src/client/actions/action-creators";
 import Piece from "../../../src/server/data/piece/Piece";
 import {expect} from "chai";
+import {GRID_WIDTH, GRID_HEIGHT} from "../../../src/common/grid";
+import {PIECES_MOVE} from "../../../src/common/pieces";
 
 describe('reducer', () => {
   describe('#reducerPiecesFlow', () => {
@@ -10,43 +15,32 @@ describe('reducer', () => {
       store.dispatch(addPiecesFlow([new Piece(2, 1, {x:0, y:0})]));
       expect(store.getState().piecesFlow[0].num).to.equal(2);
 
-      /*
-      let newState = reducer(getState(), addPiecesFlow([{
-        num: 2,
-        rot: 3,
-        pos: {x: 2, y: 10},
-      }]));
-      newState = reducer(getState(), addError({
+      store.dispatch(addError({
         message: "msg",
         type: "type",
       }));
-      expect(newState.error.message).to.equal("msg");
-      expect(newState.error.type).to.equal("type");
+      expect(store.getState().error.message).to.equal("msg");
+      expect(store.getState().error.type).to.equal("type");
 
-      newState = reducer(getState(), updatePlayers([{
-        num: 2,
-        rot: 3,
-        pos: {x: 2, y: 10},
-      }]));
-      expect(newState.piecesFlow[2].num).to.equal(2);
+      //store.dispatch(updatePlayers());
+      //expect(store.getState()).to.equal(2);
 
-      newState = reducer(getState(), movePiece(PIECES_MOVE.DOWN));
-      expect(newState.piecesFlow[0].pos.x).to.equal(1);
+      store.dispatch(movePiece(PIECES_MOVE.DOWN));
+      //need expect
 
       let grid = Array(GRID_HEIGHT).fill(0).map(() => Array(GRID_WIDTH).fill(1));
-      newState = reducer(getState(), updateGrid(grid, getState.playerName));
-      expect(newState.playerStates[0].grid[0][0]).to.equal(1);
+      store.dispatch(updateGrid(grid, store.getState().playerName));
+      expect(store.getState().playerStates[0].grid[0][0]).to.equal(1);
 
-      newState = reducer(getState(), startGame([{
+      store.dispatch(startGame([{
         num: 2,
         rot: 3,
         pos: {x: 2, y: 10},
       }]));
-      expect(newState.piecesFlow[2].num).to.equal(2);
+      expect(store.getState().piecesFlow[0].num).to.equal(2);
 
-      newState = reducer(getState(), addWallLine());
-      //expect(newState.piecesFlow[2].num).to.equal(2);
-      */
+      store.dispatch(addWallLine());
+      expect(store.getState().playerStates[0].grid[GRID_HEIGHT - 1][0]).to.equal(-1);
     });
   });
 });
