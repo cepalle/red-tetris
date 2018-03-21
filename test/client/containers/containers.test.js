02da4bdeb93b7a5e5be4bfd31de44ed0d7e3f74a
cepalle <epalle.cyrille@gmail.com>
2018-03-21 18:33:58 +0100

    keyEvent bug, start mount component

---
+++ test/client/containers/containers.test.js
@@ -0,0 +1,23 @@
+/*
+import {ButtonTest} from "../../../src/client/containers/button-test";
+import React from 'react';
+import {mount} from 'enzyme';
+import {store} from "../../../src/client/middlewares/store";
+
+// setup file
+import {configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({adapter: new Adapter()});
+
+describe("component", () => {
+  it('ButtonTest', () => {
+
+    const context = {
+      store,
+    };
+    mount(<ButtonTest/>, {context});
+
+  });
+});
+*/
--- test/client/util/event-listener-handler.test.js
+++ test/client/util/event-listener-handler.test.js
@@ -1,19 +1,27 @@
 import "../../../src/client/util/event-listener-handler"
+/*
+describe('event-listener-handler', () => {
+  describe('#event-listener-handler', () => {
+    it('should sendEvent ', () => {
 
-let event = new CustomEvent('keydown');
+      let event = new CustomEvent('keydown');
 
-event.keyCode = 37;
-window.dispatchEvent(event);
+      event.keyCode = 37;
+      window.dispatchEvent(event);
 
-event.keyCode = 38;
-window.dispatchEvent(event);
+      event.keyCode = 38;
+      window.dispatchEvent(event);
 
-event.keyCode = 39;
-window.dispatchEvent(event);
+      event.keyCode = 39;
+      window.dispatchEvent(event);
 
-event.keyCode = 40;
-window.dispatchEvent(event);
+      event.keyCode = 40;
+      window.dispatchEvent(event);
 
-event.keyCode = 32;
-window.dispatchEvent(event);
+      event.keyCode = 32;
+      window.dispatchEvent(event);
 
+    });
+  });
+});
+*/
