852e990ffbb338b1dc94b49925f051e7632193ba
cepalle <epalle.cyrille@gmail.com>
2018-03-23 09:50:54 +0100

    can know deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
 module.exports = {
+  entry: './src/client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
   module: {
     rules: [
       {
