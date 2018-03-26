65b11b37d6b375eb9f701da4c1fd16cab743d45f
cepalle <epalle.cyrille@gmail.com>
2018-03-26 15:47:39 +0200

    move audio player

--- src/client/components/app.js
+++ src/client/components/app.js
@@ -2,11 +2,9 @@ import React from "react";
 import {GridPlayer} from "../containers/grid-player"
 import {InfoPanel} from "../containers/info-panel";
 import {Opponents} from "../containers/opponents";
-import mp3 from '../assets/Original_Tetris_theme.mp3'
 
 const App = () =>
   <div className={"column"}>
-    <audio loop autoPlay src={mp3}/>
     <div className={"row center"}>
       <div className={"row color8 pad"}>
         <InfoPanel/>
--- src/client/containers/info-panel.js
+++ src/client/containers/info-panel.js
@@ -1,55 +1,66 @@
 import React from "react";
 import {connect} from 'react-redux';
 import {sendStartGame} from "../actions/action-creators";
+import mp3 from '../assets/Original_Tetris_theme.mp3'
 
 const InfoPanelComponent = ({error, animate, master, playerName, roomName, onClickButton, canStart}) =>
-  <div className={"column width_info_panel"}>
-    <div className={"row center"}>
-      <h1 className={"font_white font_retro"}>TETRIS</h1>
+  <div className={"column width_info_panel spaceBetween"}>
+    <div className={"row"}>
+      <div className={"column"}>
+        <div className={"row center"}>
+          <h1 className={"font_white font_retro"}>TETRIS</h1>
+        </div>
+        <div className={"font_white pad"}>
+          {"For play you need add to the url: #<roomName>[<yourPseudo>]"}<br/>
+          {"> Exemple: cepalle.github.io/red-tetris/#chateauHyrule[Zelda]"}<br/>
+          {"Refresh the page with the new url"}<br/><br/>
+          {"<keyRight>: move right"}<br/>
+          {"<keyLeft>: move left"}<br/>
+          {"<keyDown>: move down"}<br/>
+          {"<keyUp>: rotate right"}<br/>
+          {"<keySpace>: place the piece"}<br/>
+          {"<keyEnter>: start the game if your are master"}<br/>
+
+          {playerName && roomName && master &&
+          <div className={"pad"}>
+            <button className={"font_retro font_white font_button buttonPlay"} onClick={() => onClickButton()}>
+              Play!
+            </button>
+          </div>}
+
+
+          {playerName && roomName && !animate && master &&
+          <p className={"font_green"}>{"You are currently waiting for other players"}<br/></p>}
+
+          {playerName && roomName && !animate && !master &&
+          <p className={"font_green"}>{"You are currently waiting for master player"}<br/></p>}
+
+          {(!playerName || !roomName) &&
+          <p className={"font_red"}>{"You need to join a room and take a pseudo"}<br/>
+            {"(The room will be created if doesn't exist)"}<br/></p>}
+
+
+          {error.type === "PLAYER_ALREADY_IN_ROOM" &&
+          <p className={"font_red"}>{"A player as already your pseudo in this room"}<br/></p>}
+
+          {error.type && error.type === "ROOM_ALREADY_IN_GAME" &&
+          <p className={"font_red"}>{"Others players are still playing"}<br/></p>}
+
+          {error.type && error.type === "PLAYER_NOT_MASTER" &&
+          <p className={"font_red"}>{"You need to be master for start the game"}<br/></p>}
+
+          {error.type && error.type !== "PLAYER_ALREADY_IN_ROOM" &&
+          error.type !== "PLAYER_NOT_MASTER" &&
+          error.type !== "ROOM_ALREADY_IN_GAME" &&
+          <p className={"font_red"}>{error.type}<br/></p>}
+
+        </div>
+      </div>
     </div>
-    <div className={"font_white pad"}>
-      {"For play you need add to the url: #<roomName>[<yourPseudo>]"}<br/>
-      {"> Exemple: cepalle.github.io/red-tetris/#chateauHyrule[Zelda]"}<br/>
-      {"Refresh the page with the new url"}<br/><br/>
-      {"<keyRight>: move right"}<br/>
-      {"<keyLeft>: move left"}<br/>
-      {"<keyDown>: move down"}<br/>
-      {"<keyUp>: rotate right"}<br/>
-      {"<keySpace>: place the piece"}<br/>
-      {"<keyEnter>: start the game if your are master"}<br/>
-
-      {playerName && roomName && master &&
-      <div className={"pad"}>
-        <button className={"font_retro font_white font_button buttonPlay"} onClick={() => onClickButton()}>
-          Play!
-        </button>
-      </div>}
-
-      {playerName && roomName && !animate && master &&
-      <p className={"font_green"}>{"You are currently waiting for other players"}<br/></p>}
-
-      {playerName && roomName && !animate && !master &&
-      <p className={"font_green"}>{"You are currently waiting for master player"}<br/></p>}
-
-      {(!playerName || !roomName) &&
-      <p className={"font_red"}>{"You need to join a room and take a pseudo"}<br/>
-        {"(The room will be created if doesn't exist)"}<br/></p>}
-
-
-      {error.type === "PLAYER_ALREADY_IN_ROOM" &&
-      <p className={"font_red"}>{"A player as already your pseudo in this room"}<br/></p>}
-
-      {error.type && error.type === "ROOM_ALREADY_IN_GAME" &&
-      <p className={"font_red"}>{"Others players are still playing"}<br/></p>}
-
-      {error.type && error.type === "PLAYER_NOT_MASTER" &&
-      <p className={"font_red"}>{"You need to be master for start the game"}<br/></p>}
-
-      {error.type && error.type !== "PLAYER_ALREADY_IN_ROOM" &&
-      error.type !== "PLAYER_NOT_MASTER" &&
-      error.type !== "ROOM_ALREADY_IN_GAME" &&
-      <p className={"font_red"}>{error.type}<br/></p>}
-
+    <div className={"row"}>
+      <div className={"column"}>
+        <audio controls loop autoPlay src={mp3} className={"color0"}/>
+      </div>
     </div>
   </div>
 ;
