38a0b3ed9cdb1fed20e9779cc461b01060a8c503
cepalle <epalle.cyrille@gmail.com>
2018-12-22 07:32:27 +0100

    add interface action

--- src/client/actions/action-creators.ts
+++ src/client/actions/action-creators.ts
@@ -1,180 +1,251 @@
-/**
- * Create action for add pieces to the state.piecesFlow.
- * @param {[pieces]} pieces
- */
-const ADD_PIECES_FLOW = pieces => {
+enum EnumAction {
+  ADD_PIECES_FLOW,
+  ADD_ERROR,
+  UPDATE_PLAYERS,
+  PIECES_MOVE,
+  UPDATE_GRID,
+  RECV_START_GAME,
+  ADD_WALL_LINE,
+  CONNECTION_RESPONSE,
+  SEND_START_GAME,
+  UPDATE_ROOM_PLAYER_NAME,
+  UPDATE_GAMES,
+  EMIT_QUIT_GAME,
+  TOGGLE_ADD_WALL_LINE,
+  TOGGLE_GROUND_RESIZER,
+  UPDATE_EMITE_LOOSE,
+  UPDATE_EMITE_JOIN_ROOM,
+  UPDATE_EMITE_UPDATE_GRID,
+  UPDATE_EMITE_COMPLETE_LINE,
+  CLEAN_ERROR,
+  UPDATE_SOCKET_IS_CONNECT,
+}
+
+interface IAction {
+  type: EnumAction
+}
+
+// ---
+
+interface IAddPiecesFlow extends IAction {
+  type: EnumAction.ADD_PIECES_FLOW
+  pieces: any[] // TODO
+}
+
+const ADD_PIECES_FLOW = (pieces: any[]): IAddPiecesFlow => {
   return {
-    type: 'ADD_PIECES_FLOW',
+    type: EnumAction.ADD_PIECES_FLOW,
     pieces: pieces
   };
 };
 
-/**
- * Create a action for set error to getState.error.
- * @param {type, message} error
- */
-const ADD_ERROR = error => {
+interface IAddError extends IAction {
+  type: EnumAction.ADD_ERROR,
+  error: any // TODO
+}
+
+const ADD_ERROR = (error: any): IAddError => {
   return {
-    type: 'ADD_ERROR',
+    type: EnumAction.ADD_ERROR,
     error: error
   }
 };
 
-/**
- * Create a action for synchronize players with users.
- * @param {Array<player>} players
- */
-const UPDATE_PLAYERS = players => {
+interface IUpadtePlayers extends IAction {
+  type: EnumAction.UPDATE_PLAYERS,
+  players: any[]
+}
+
+const UPDATE_PLAYERS = (players: any[]): IUpadtePlayers => {
   return {
-    type: 'UPDATE_PLAYERS',
+    type: EnumAction.UPDATE_PLAYERS,
     players: players
   }
 };
 
-/**
- * Create a action for update the grid with the move of the part.
- * @param {string} move
- */
-const PIECES_MOVE = move => {
+interface IPiecesMove extends IAction {
+  type: EnumAction.PIECES_MOVE,
+  move: any
+}
+
+const PIECES_MOVE = (move: any): IPiecesMove => {
   return {
-    type: 'PIECES_MOVE',
+    type: EnumAction.PIECES_MOVE,
     move: move
   }
 };
 
-/**
- * Create a action for update the grid of the player that as change.
- * @param {string} playerName
- * @param {Array<Array<int>>} grid
- */
-const UPDATE_GRID = (grid, playerName) => {
+interface IUpdateGrid extends IAction {
+  type: EnumAction.UPDATE_GRID,
+  grid: any,
+  playerName: any,
+}
+
+const UPDATE_GRID = (grid: any, playerName: any): IUpdateGrid => {
   return {
-    type: 'UPDATE_GRID',
+    type: EnumAction.UPDATE_GRID,
     grid: grid,
     playerName: playerName
   }
 };
 
-/**
- * Restart grid of player and flow, set le pieces to the flow and start game.
- * @param {[pieces]} pieces
- * @param {Params} params
- */
-const RECV_START_GAME = (pieces, params) => {
+interface IRecvStartGame extends IAction {
+  type: EnumAction.RECV_START_GAME,
+  pieces: any,
+  params: any,
+}
+
+const RECV_START_GAME = (pieces: any, params: any): IRecvStartGame => {
   return {
-    type: 'RECV_START_GAME',
+    type: EnumAction.RECV_START_GAME,
     pieces: pieces,
     params: params
   }
 };
 
-/**
- * Add a line unbreakable.
- */
-const ADD_WALL_LINE = (amount) => {
+interface IAddWallLine extends IAction {
+  type: EnumAction.ADD_WALL_LINE,
+  amount: any
+}
+
+const ADD_WALL_LINE = (amount: any): IAddWallLine => {
   return {
-    type: 'ADD_WALL_LINE',
+    type: EnumAction.ADD_WALL_LINE,
     amount: amount
   }
 };
 
-/**
- * Add a line unbreakable.
- */
-const CONNECTION_RESPONSE = () => {
+interface IConnectionResponse extends IAction {
+  type: EnumAction.CONNECTION_RESPONSE
+}
+
+const CONNECTION_RESPONSE = (): IConnectionResponse => {
   return {
-    type: 'CONNECTION_RESPONSE'
+    type: EnumAction.CONNECTION_RESPONSE
   }
 };
 
-/**
- * Add a line unbreakable.
- */
-const SEND_START_GAME = () => {
+interface ISendStartGame extends IAction {
+  type: EnumAction.SEND_START_GAME
+}
+
+const SEND_START_GAME = (): ISendStartGame => {
   return {
-    type: 'SEND_START_GAME'
+    type: EnumAction.SEND_START_GAME
   }
 };
 
-/**
- * update room and player name.
- */
-const UPDATE_ROOM_PLAYER_NAME = (roomName, playerName) => {
+interface IUpdateRoomPlayerName extends IAction {
+  type: EnumAction.UPDATE_ROOM_PLAYER_NAME,
+  roomName: any,
+  playerName: any
+}
+
+const UPDATE_ROOM_PLAYER_NAME = (roomName: any, playerName: any): IUpdateRoomPlayerName => {
   return {
-    type: 'UPDATE_ROOM_PLAYER_NAME',
+    type: EnumAction.UPDATE_ROOM_PLAYER_NAME,
     roomName: roomName,
     playerName: playerName
   }
 };
 
-/**
- * update rooms name for home.
- */
-const UPDATE_GAMES = games => {
+interface IUpdateGame extends IAction {
+  type: EnumAction.UPDATE_GAMES,
+  games: any
+}
+
+const UPDATE_GAMES = (games: any): IUpdateGame => {
   return {
-    type: 'UPDATE_GAMES',
+    type: EnumAction.UPDATE_GAMES,
     games: games,
   }
 };
 
-/**
- * send to the server QUIT_GAME.
- */
-const EMIT_QUIT_GAME = () => {
+interface IEmitQuitGame extends IAction {
+  type: EnumAction.EMIT_QUIT_GAME
+}
+
+const EMIT_QUIT_GAME = (): IEmitQuitGame => {
   return {
-    type: 'EMIT_QUIT_GAME',
+    type: EnumAction.EMIT_QUIT_GAME,
   }
 };
 
-/**
- * send to the server QUIT_GAME.
- */
-const TOGGLE_ADD_WALL_LINE = () => {
+interface IToggleAddWallLine extends IAction {
+  type: EnumAction.TOGGLE_ADD_WALL_LINE
+}
+
+const TOGGLE_ADD_WALL_LINE = (): IToggleAddWallLine => {
   return {
-    type: 'TOGGLE_ADD_WALL_LINE',
+    type: EnumAction.TOGGLE_ADD_WALL_LINE,
   }
 };
 
-/**
- * send to the server QUIT_GAME.
- */
-const TOGGLE_GROUND_RESIZER = () => {
+interface IToggleGroundResizer extends IAction {
+  type: EnumAction.TOGGLE_GROUND_RESIZER
+}
+
+const TOGGLE_GROUND_RESIZER = (): IToggleGroundResizer => {
   return {
-    type: 'TOGGLE_GROUND_RESIZER',
+    type: EnumAction.TOGGLE_GROUND_RESIZER,
   }
 };
 
-const UPDATE_EMITE_LOOSE = (bool) => {
+interface IUpdateEmitLoose extends IAction {
+  type: EnumAction.UPDATE_EMITE_LOOSE,
+  bool: boolean
+}
+
+const UPDATE_EMITE_LOOSE = (bool: boolean): IUpdateEmitLoose => {
   return {
-    type: 'UPDATE_EMITE_LOOSE',
+    type: EnumAction.UPDATE_EMITE_LOOSE,
     bool
   }
 };
 
-const UPDATE_EMITE_JOIN_ROOM = (bool) => {
+interface IUpdateEmiteJoinRoom extends IAction {
+  type: EnumAction.UPDATE_EMITE_JOIN_ROOM,
+  bool: boolean
+}
+
+const UPDATE_EMITE_JOIN_ROOM = (bool: boolean): IUpdateEmiteJoinRoom => {
   return {
-    type: 'UPDATE_EMITE_JOIN_ROOM',
+    type: EnumAction.UPDATE_EMITE_JOIN_ROOM,
     bool
   }
 };
 
-const UPDATE_EMITE_UPDATE_GRID = (bool) => {
+interface IUpdateEmiteUpdateGrid extends IAction {
+  type: EnumAction.UPDATE_EMITE_UPDATE_GRID,
+  bool: boolean
+}
+
+const UPDATE_EMITE_UPDATE_GRID = (bool: boolean): IUpdateEmiteUpdateGrid => {
   return {
-    type: 'UPDATE_EMITE_UPDATE_GRID',
+    type: EnumAction.UPDATE_EMITE_UPDATE_GRID,
     bool
   }
 };
 
-const UPDATE_EMITE_COMPLETE_LINE = (nb) => {
+interface IUpdateEmiteCompleteLine extends IAction {
+  type: EnumAction.UPDATE_EMITE_COMPLETE_LINE,
+  nb: number
+}
+
+const UPDATE_EMITE_COMPLETE_LINE = (nb: number): IUpdateEmiteCompleteLine => {
   return {
-    type: 'UPDATE_EMITE_COMPLETE_LINE',
+    type: EnumAction.UPDATE_EMITE_COMPLETE_LINE,
     nb
   }
 };
 
-const CLEAN_ERROR = () => {
+interface ICleanError extends IAction {
+  type: EnumAction.CLEAN_ERROR
+}
+
+const CLEAN_ERROR = (): ICleanError => {
   return {
-    type: 'CLEAN_ERROR'
+    type: EnumAction.CLEAN_ERROR
   }
 };
 
