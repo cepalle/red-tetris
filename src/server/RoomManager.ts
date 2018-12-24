4049c16b1b8dc404bb9bf5d04ab281bf9b979ba3
cepalle <epalle.cyrille@gmail.com>
2018-12-24 17:54:55 +0100

    start use reducer roomManager

--- src/server/RoomManager.ts
+++ src/server/RoomManager.ts
@@ -4,6 +4,130 @@ import {BehaviorSubject} from 'rxjs/internal/BehaviorSubject';
 import {IEventPlacePiece} from '@src/common/socketEventServer';
 import {factPlayer} from '@src/server/playerUtils';
 
+// ACTION
+
+enum EnumActionRoomStore {
+  ADD_PLAYER,
+  DEL_PLAYER,
+  UPDATE_OPTION_GAME,
+  START_GAME,
+}
+
+interface IActionRoom {
+  type: EnumActionRoomStore;
+}
+
+// ADD_PLAYER
+
+interface IActionRoomAddPlayer extends IActionRoom {
+  type: EnumActionRoomStore.ADD_PLAYER;
+
+  playerName: string;
+  socket: Socket;
+}
+
+const reducerAddPlayer = (state: IRoomState, action: IActionRoomAddPlayer): IRoomState => {
+
+  const {playerName, socket} = action;
+
+  const hasPlayerName = state.players.some((p) => p.playerName === playerName);
+  if (hasPlayerName) {
+    // TODO emit error
+  }
+
+  const hasSocket = state.players.some((p) => p.socket.id === socket.id);
+  if (hasSocket) {
+    // TODO
+  }
+
+  const isMaster = state.players.length === 0;
+  return {
+    ...state,
+    players: [...state.players,
+      factPlayer(playerName, socket, isMaster),
+    ],
+  };
+};
+
+// DEL_PLAYER
+
+interface IActionRoomDelPlayer extends IActionRoom {
+  type: EnumActionRoomStore.DEL_PLAYER;
+
+  socketId: string;
+}
+
+const reducerDelPlayer = (state: IRoomState, action: IActionRoomDelPlayer): IRoomState => {
+
+  const {socketId} = action;
+
+  if (state.players.some((p) => p.socket.id === socketId)) {
+    return {
+      ...state,
+      players: state.players.filter((p) => p.socket.id === socketId),
+    };
+  }
+  return state;
+};
+
+// UPDATE_OPTION_GAME
+
+interface IActionUpdateOptionGame extends IActionRoom {
+  type: EnumActionRoomStore.UPDATE_OPTION_GAME;
+
+  optionGame: IOptionGame;
+}
+
+const reducerUpdateOptionGame = (state: IRoomState, action: IActionUpdateOptionGame): IRoomState => {
+
+  const {optionGame} = action;
+
+  return {
+    ...state,
+    optionGame: optionGame,
+  };
+};
+
+// START_GAME
+
+interface IActionStartGame extends IActionRoom {
+  type: EnumActionRoomStore.START_GAME;
+}
+
+const reducerStartGame = (state: IRoomState, action: IActionStartGame): IRoomState => {
+  return {
+    ...state,
+    playing: true,
+  };
+};
+
+// ACTION ROOM
+
+type ActionRoom = IActionRoomAddPlayer
+  | IActionRoomDelPlayer
+  | IActionUpdateOptionGame
+  | IActionStartGame;
+
+// REDUCER
+
+// use middelware for check end send socket... ?
+const reducer = (state: IRoomState, action: ActionRoom): IRoomState => {
+  switch (action.type) {
+    case EnumActionRoomStore.ADD_PLAYER:
+      return reducerAddPlayer(state, action);
+    case EnumActionRoomStore.DEL_PLAYER:
+      return reducerDelPlayer(state, action);
+    case EnumActionRoomStore.UPDATE_OPTION_GAME:
+      return reducerUpdateOptionGame(state, action);
+    case EnumActionRoomStore.START_GAME:
+      return reducerStartGame(state, action);
+    default:
+      return state;
+  }
+};
+
+// ISTATE
+
 interface IPlayer {
   playerName: string;
   socket: Socket;
@@ -24,6 +148,8 @@ interface IRoomState {
   optionGame: IOptionGame;
 }
 
+// ROOM MANAGER
+
 class RoomManager {
 
   state: IRoomState;
@@ -42,63 +168,22 @@ class RoomManager {
     this.stateSub = new BehaviorSubject<IRoomState>(this.state);
   }
 
-  public addPlayer(playerName: string, socket: Socket): void {
-    const hasPlayerName = this.state.players.some((p) => p.playerName === playerName);
-    if (hasPlayerName) {
-      // TODO emit error
-    }
+  public dispatch(action: ActionRoom) {
+    const newState = reducer(this.state, action);
 
-    const hasSocket = this.state.players.some((p) => p.socket.id === socket.id);
-    if (hasSocket) {
-      // TODO
-    }
-
-    const isMaster = this.state.players.length === 0;
-    this.updateState({
-      ...this.state,
-      players: [...this.state.players,
-        factPlayer(playerName, socket, isMaster),
-      ],
-    });
-  }
-
-  public delPlayer(socket: Socket): void {
-    if (this.state.players.some((p) => p.socket.id === socket.id)) {
-      this.updateState({
-        ...this.state,
-        players: this.state.players.filter((p) => p.socket.id === socket.id),
-      });
+    if (newState !== this.state) {
+      this.state = newState;
+      this.stateSub.next(this.state);
     }
   }
 
-  public hasSock(socket: Socket) {
-    return this.state.players.some((p) => p.socket.id === socket.id);
-  }
-
-  public updateOptionGame(optionGame: IOptionGame) {
-    this.updateState({
-      ...this.state,
-      optionGame: optionGame,
-    });
-  }
-
-  public startGame() {
-    this.updateState({
-      ...this.state,
-      playing: true,
-    });
+  public hasSocketId(socketId: string) {
+    return this.state.players.some((p) => p.socket.id === socketId);
   }
 
   public placePiece(socket: Socket, arg: IEventPlacePiece) {
     // const {piece, pos} = arg;
     // TODO
-  }
-
-  // Private
-
-  private updateState(state: IRoomState) {
-    this.state = state;
-    this.stateSub.next(this.state);
   }
 
 }
