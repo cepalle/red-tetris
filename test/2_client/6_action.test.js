6279a611d2ba5c76a4766e651834dfb4b4c6b43d
cepalle <epalle.cyrille@gmail.com>
2018-12-26 16:04:33 +0100

    del test

--- test/2_client/6_action.test.js
+++
@@ -1,8 +0,0 @@
-import {ADD_WALL_LINE, UPDATE_GRID} from "../../src/client/actions/action-creators";
-
-describe('action test', () => {
-  it('reducerUpdateGrid', () => {
-    UPDATE_GRID([], "playerName");
-    ADD_WALL_LINE(8);
-  });
-});
--- test/2_client/7_annimate.test.js
+++
@@ -1,8 +0,0 @@
-import "../../src/client/util/animate-handler";
-import {animateClock} from "../../src/client/util/animate-handler";
-
-describe('animate test', () => {
-  it('animateClock', () => {
-    animateClock();
-  });
-});
