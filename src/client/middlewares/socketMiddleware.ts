068165358fdae1385d48b415a0265a987dec2d96
cepalle <epalle.cyrille@gmail.com>
2018-12-26 12:55:20 +0100

    update middelware

--- src/client/middlewares/socketMiddleware.ts
+++ src/client/middlewares/socketMiddleware.ts
@@ -1,16 +1,34 @@
 import {EnumAction, ReduxAction} from '@src/client/actions/action-creators';
-import Socket = SocketIOClient.Socket;
-import {ENUM_SOCKET_EVENT_SERVER} from '@src/common/socketEventServer';
+import {ENUM_SOCKET_EVENT_SERVER, IEventSetGameOption, IEventStartGame} from '@src/common/socketEventServer';
 
-const sendStartGame = (socket: Socket) => {
-  socket.emit(ENUM_SOCKET_EVENT_SERVER.START_GAME, {});
+const sendStartGame = (socket: SocketIOClient.Socket, arg: IEventStartGame): void => {
+  socket.emit(ENUM_SOCKET_EVENT_SERVER.START_GAME, arg);
+};
+
+const sendUpdateOptionGame = (socket: SocketIOClient.Socket, arg: IEventSetGameOption): void => {
+  socket.emit(ENUM_SOCKET_EVENT_SERVER.START_GAME, arg);
 };
 
 const socketMiddleware = (store: any) => (next: any) => (action: ReduxAction) => {
+
+  const state = store.getState();
+
   switch (action.type) {
     case EnumAction.SEND_START_GAME:
-      sendStartGame(store.getState().socket);
-      return next(action);
+      if (state.socket !== undefined && state.roomName !== undefined) {
+        sendStartGame(state.socket, {
+          roomName: state.roomName,
+        });
+      }
+      break;
+    case EnumAction.SEND_UPDATE_OPTION_GAME:
+      if (state.socket !== undefined && state.roomName !== undefined) {
+        sendUpdateOptionGame(state.socket, {
+          roomName: state.roomName,
+          optionGame: action.optionGame,
+        });
+      }
+      break;
     default:
       break;
   }
--- src/common/socketEventServer.ts
+++ src/common/socketEventServer.ts
@@ -1,22 +1,26 @@
 import {IOptionGame, IPiece, IPos} from '@src/common/IType';
 
+// SET_ROOM_PLAYER_NAME
 interface IEventSetRoomPlayerName {
   roomName: string,
 
   playerName: string,
 }
 
+// SET_GAME_OPTION
 interface IEventSetGameOption {
   roomName: string,
 
   optionGame: IOptionGame
 }
 
+// START_GAME
 interface IEventStartGame {
   roomName: string,
 
 }
 
+// PLACE_PIECE
 interface IEventPlacePiece {
   roomName: string,
 
