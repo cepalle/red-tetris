0c05a66847f26e207f2b91b508010cca708ef2ed
cepalle <epalle.cyrille@gmail.com>
2019-02-05 10:50:23 +0100

    update test 69.8

--- test/2_client/3_reducer.test.js
+++ test/2_client/3_reducer.test.js
@@ -1,25 +1,45 @@
 import {store} from "../../src/client/middlewares/store";
-import {reducerUpdateGrid, reducerAddWallLine, reducerUpdatePlayers} from "../../src/client/reducers/reducer-aux";
+import {
+  reducerUpdateGrid,
+  reducerAddWallLine,
+  reducerUpdatePlayers,
+  reducerUpdateGames, reducerStartGame
+} from "../../src/client/reducers/reducer-aux";
 import {initPlayerState} from "../../src/client/reducers/reducer"
 
 describe('reducer test', () => {
   it('reducerUpdateGrid', () => {
     const obj = {
-      grid:undefined,
+      grid: undefined,
       playerName: store.getState().playerName
     };
     reducerUpdateGrid(store.getState(), obj);
   });
+
   it('reducerAddWallLine', () => {
     const obj = {
       amount: 1
     };
     reducerAddWallLine(store.getState(), obj);
   });
+
   it('reducerUpdatePlayers', () => {
     const obj = {
       players: store.getState().playerStates.concat(initPlayerState("tetere", false)),
     };
     reducerUpdatePlayers(store.getState(), obj);
+  });
+
+  it('reducerUpdateGames', () => {
+    reducerUpdateGames(store.getState(), {});
+  });
+
+  it('reducerStartGame', () => {
+    reducerStartGame(store.getState(), {
+      params: {
+        groundResizer: true
+      },
+      pieces: []
+    });
   });
 });
